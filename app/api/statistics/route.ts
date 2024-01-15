1676ad738f369af5f630f823af6170f9cae78873
Visinigiri Aditya <adityakumarverified@gmail.com>
2024-01-15 17:28:51 +0530

    fix - ui design and dynamic in statictics page

--- app/api/statistics/route.ts
+++ app/api/statistics/route.ts
@@ -1,27 +1,80 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
-import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { Task } from "@prisma/client";
 
 export const dynamic = "force-dynamic";
+
+const calculateTime = (tasks: Task[]) => {
+  let totalTime = 0;
+  tasks.forEach((task) => {
+    const time = task.Time;
+    const timeArray = time.split(":");
+    const hours = parseInt(timeArray[0]);
+    const minutes = parseInt(timeArray[1]);
+    const seconds = parseInt(timeArray[2]);
+    totalTime += hours * 3600 + minutes * 60 + seconds;
+  });
+
+  const hours = Math.floor(totalTime / 3600);
+  const minutes = Math.floor((totalTime % 3600) / 60);
+  const seconds = Math.floor((totalTime % 3600) % 60);
+
+  return {
+    hours,
+    minutes,
+    seconds,
+  };
+};
 
 export async function GET(request: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
 
     const tasks = await prisma.task.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
       where: {
         userId: session?.user.id!,
       },
     });
-    console.log(tasks);
 
-    return NextResponse.json(tasks);
-  } catch (error) {
-    console.log(error);
+    const reminder = await prisma.reminder.findMany({
+      where: {
+        userId: session?.user.id!,
+      },
+    });
+
+    const completedTasks = await prisma.task.findMany({
+      where: {
+        Status: "COMPLETED",
+        userId: session?.user.id!,
+      },
+    });
+
+    const calculatedCompletedTaskTime = calculateTime(completedTasks);
+
+    const notes = await prisma.notes.findMany({
+      where: {
+        userId: session?.user.id!,
+      },
+    });
+
+    // count of tasks
+    const taskCount = tasks.length;
+    // count of reminders
+    const reminderCount = reminder.length;
+    // count of notes
+    const notesCount = notes.length;
+
+    return NextResponse.json({
+      taskCount,
+      reminderCount,
+      notesCount,
+      completedTasks: completedTasks.length,
+      completedTaskTime: calculatedCompletedTaskTime,
+    });
+  } catch (err) {
+    console.log(err);
     return NextResponse.json(500);
   }
 }
--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,20 +1,44 @@
 "use client";
-import { Card, Flex, Text } from "@radix-ui/themes";
+import { Flex, Text } from "@radix-ui/themes";
 import React, { useEffect, useState } from "react";
 import LastTasks from "./components/LastTasks";
 import axios from "axios";
 import { Task } from "@prisma/client";
+import Card from "./components/Card";
 
 export const dynamic = "force-dynamic";
+
+interface Statistics {
+  taskCount: number;
+  reminderCount: number;
+  notesCount: number;
+  completedTasks: number;
+  completedTaskTime: {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+}
 
 const SatisticsPage = () => {
   const [tasks, setTasks] = useState<Task[]>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [statistics, setStatistics] = useState<Statistics>({
+    taskCount: 0,
+    reminderCount: 0,
+    notesCount: 0,
+    completedTasks: 0,
+    completedTaskTime: {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    },
+  });
 
   const getTasks = async () => {
     try {
       setIsLoading(true);
-      const res = await axios.get("/api/statistics");
+      const res = await axios.get("/api/statistics/tasks");
       const data = (await res.data) as Task[];
       setTasks(data);
       setIsLoading(false);
@@ -26,25 +50,85 @@ const SatisticsPage = () => {
     }
   };
 
+  const getStatistics = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.get("/api/statistics");
+      const data = await res.data;
+      setStatistics(data);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
+    getStatistics();
     getTasks();
   }, []);
 
   return (
     <div className="md:px-16 px-8 mt-8">
-      <div className="md:flex gap-2  text-neutral-800 justify-between px-8">
-        <Card style={{ width: 200, height: 140, marginBottom: 20 }}>
+      <div className=" md:flex mx-auto justify-center text-neutral-800  gap-5  ">
+        <Card
+          classname="bg-purple-50 hover:border-purple-400 text-xl "
+          title="Tasks"
+          item={statistics?.taskCount as any}
+        />
+        <Card
+          classname="bg-teal-50 hover:border-teal-400 text-lg"
+          title="Completed Tasks"
+          item={statistics?.completedTasks as any}
+        />
+        <Card
+          classname="bg-indigo-50 hover:border-indigo-400 text-xl"
+          title="Notes"
+          item={statistics?.notesCount as any}
+        />
+        <Card
+          classname="bg-fuchsia-50 hover:border-fuchsia-400 text-xl"
+          title="Reminders"
+          item={statistics?.reminderCount as any}
+        />
+        <Card
+          classname="bg-pink-50 hover:border-pink-400 text-lg"
+          title="Total Task Time"
+          item={
+            `${statistics?.completedTaskTime.hours}h: ${statistics?.completedTaskTime.minutes}m: ${statistics?.completedTaskTime.seconds}s` as any
+          }
+        />
+        {/* <Card style={{ width: 150, height: 140, marginBottom: 20 }}>
           <Flex
             direction="column"
             align={"center"}
             height={"100%"}
             justify={"center"}
           >
-            <Text className="text-2xl font-poppins  ">4</Text>
-            <Text className="text-2xl font-poppins font-semibold ">Tasks</Text>
+            <Text className="text-2xl font-poppins  ">
+              {statistics?.taskCount}
+            </Text>
+            <Text className="text-xl font-poppins font-semibold ">Tasks</Text>
           </Flex>
         </Card>
-        <Card style={{ width: 200, height: 140, marginBottom: 20 }}>
+        <Card style={{ width: 150, height: 140, marginBottom: 20 }}>
+          <Flex
+            direction="column"
+            align={"center"}
+            height={"100%"}
+            justify={"center"}
+          >
+            <Text className="text-2xl font-poppins  ">
+              {statistics?.taskCount}
+            </Text>
+            <Text className="text-xl font-poppins font-semibold ">
+              Completed Tasks
+            </Text>
+          </Flex>
+        </Card>
+        <Card style={{ width: 150, height: 140, marginBottom: 20 }}>
           <Flex
             direction="column"
             align={"center"}
@@ -55,7 +139,7 @@ const SatisticsPage = () => {
             <Text className="text-2xl font-poppins font-semibold ">Notes</Text>
           </Flex>
         </Card>
-        <Card style={{ width: 200, height: 140, marginBottom: 20 }}>
+        <Card style={{ width: 150, height: 140, marginBottom: 20 }}>
           <Flex
             direction="column"
             align={"center"}
@@ -68,7 +152,7 @@ const SatisticsPage = () => {
             </Text>
           </Flex>
         </Card>
-        <Card style={{ width: 200, height: 140, marginBottom: 20 }}>
+        <Card style={{ width: 150, height: 140, marginBottom: 20 }}>
           <Flex
             direction="column"
             align={"center"}
@@ -80,7 +164,7 @@ const SatisticsPage = () => {
               Today Time
             </Text>
           </Flex>
-        </Card>
+        </Card> */}
       </div>
 
       <LastTasks tasks={tasks as Task[]} isLoading={isLoading} />
